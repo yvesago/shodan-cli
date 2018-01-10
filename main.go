ef890bab322b29d66f2d42f2e9cc6d5c29eea3a4
yvesago <yves@vya.me>
2018-01-10 22:41:37 +0100

    add version

--- main.go
+++ main.go
@@ -13,11 +13,21 @@ import (
 	"gopkg.in/ns3777k/go-shodan.v2/shodan"
 )
 
+var version = "0.1.0"
+
 var au aurora.Aurora
 
 func parseArgs() (string, string, string, bool) {
 	var query, net, ip string
 	var compact, color bool
+
+	au = aurora.NewAurora(true)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", au.Bold(os.Args[0]))
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\n  Version: %s\n", au.Bold(version))
+	}
 
 	flag.StringVar(&query, "q", "", "query ['!http']")
 	flag.StringVar(&net, "n", "", "net [192.168.0.0/24]")
